ba9a8da73f2c6ea4fa469b33b44797c73d037bf4
Prince Singh <princesingh282002@gmail.com>
2023-02-04 00:02:42 +0530

    mailSchedular

--- emailShedular/index.js
+++ emailShedular/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { transporter, options } = require("./services/email");
 const app = express();
 const PORT = 1338;
 const schedular = require("node-cron");
 
 schedular.schedule("* * * * * *", () => {
-  console.log("inside schedular");
+  console.log("sending email. . . . . . ");
+  transporter.sendMail(options, (err, info) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("email send with info: ", info);
+  });
 });
 
 app.listen(PORT, () => {
