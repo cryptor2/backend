dc629f2204f78410058c5d31eb1c486dbaa3d30e
Prince Singh <princesingh282002@gmail.com>
2023-02-06 23:50:41 +0530

    webScapping

--- webScrapping/scrapeFn/scrape.js
+++ webScrapping/scrapeFn/scrape.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 const data = {
   list: [],
 };
@@ -10,6 +11,36 @@ async function main(skill) {
     timeout: 0,
     waitUntil: "networkidle0",
   });
+
+  const jobData = await page.evaluate(async (data) => {
+    const items = document.querySelectorAll("td.resultContent");
+    items.forEach((item, index) => {
+      const title = item.querySelector("h2.jobTitle>a")?.innerText;
+      const link = item.querySelector("h2.jobTitle>a")?.href;
+      let salary = item.querySelector(
+        "div.metadata.salary-snippt-container >div"
+      )?.innerText;
+      const companyName = item.querySelector("span.companyName")?.innerText;
+
+      if (salary === null) {
+        salary = "not defined";
+      }
+      data.list.push({
+        title,
+        salary,
+        companyName,
+        link,
+      });
+    });
+    return data;
+  }, data);
+  let response = await jobData;
+  let json =  JSON.stringify(jobData, null, 2);
+
+  fs.writeFile("job.json", json, "utf-8", () => {
+    console.log("written in job.json");
+  });
   browser.close();
+  return response;
 }
 moldule.exports = main;
